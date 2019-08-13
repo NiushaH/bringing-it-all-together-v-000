fa986e3e43fd8b1d8c38472be9105ed67458bd83
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 16:21:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,13 +50,17 @@ class Dog
   # end 
   
   def save
-    SELECT * FROM dog WHERE id = ?
-    if 
-      empty array
-      INSERT
+    if self.id
+      self.update
     else
-      found to exist
-      UPDATE      
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
   end
 end
