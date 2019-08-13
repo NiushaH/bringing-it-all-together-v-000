67d83405581f0ab00079811ca507e2d78f68e468
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 16:53:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,9 +67,9 @@ class Dog
   
   def self.new_from_db(row)
     attributes_hash = {
+      :id => row[0]
       :name =>  row[1]
       :breed => row[2]
-      :id => row[0]
     }
     new_dog = self.new(attributes_hash)
     new_dog
